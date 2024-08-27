cbc466bdecb8137632d878699ee41c0b323eb445
Syed Irtaza <syed@nexiate.com>
2024-08-27 19:48:58 +1000

    added controller test

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -1,7 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get order details with valid tokens" do
+    user = User.create(email: "user@example.com", password: "password123")
+    tokens = user.generate_tokens
+
+    get '/api/v1/orders/1', headers: {
+      "access-token": tokens[:'access-token'],
+      "client": tokens[:client],
+      "expiry": tokens[:expiry].to_s,
+      "uid": user.email
+    }, as: :json
+
+    assert_response :success
+  end
+
+  test "should not get order details with invalid tokens" do
+    get '/api/v1/orders/1', headers: {
+      "access-token": "invalid_token",
+      "client": "invalid_client",
+      "expiry": "0",
+      "uid": "user@example.com"
+    }, as: :json
+
+    assert_response :unauthorized
+  end
 end
